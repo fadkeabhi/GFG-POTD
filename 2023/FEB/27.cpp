f9d51eeb8e3664c046cae0db89013205b2a2909e
ABHISHEK FADAKE <31249309+fadkeabhi@users.noreply.github.com>
2023-02-27 22:32:05 +0530

    Create 27.cpp

---
+++ 2023/FEB/27.cpp
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+struct Node *inputList()
+{
+    int n; // length of link list
+    scanf("%d ", &n);
+
+    int data;
+    cin >> data;
+    struct Node *head = new Node(data);
+    struct Node *tail = head;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        cin >> data;
+        tail->next = new Node(data);
+        tail = tail->next;
+    }
+    return head;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+Definition for singly Link List Node
+struct Node
+{
+    int data;
+    struct Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+
+class Solution
+{
+public:
+   
+    Node *reverse(Node *head, int k)
+    {
+        Node* cur = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+        int c = 0;
+        while(cur != NULL && c < k){
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+            c++;
+        }
+        
+        if(next == NULL)  return prev;
+        
+        head->next = reverse(next, INT_MAX);
+        
+        return prev;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        struct Node *head = inputList();
+        int k;
+        cin >> k;
+
+        Solution obj;
+        Node *res = obj.reverse(head, k);
+
+        printList(res);
+    }
+}
+// } Driver Code Ends
