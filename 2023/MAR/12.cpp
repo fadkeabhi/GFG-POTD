9cbd91146d4c7c25ad4cbfdf6b6840851613de63
ABHISHEK FADAKE <31249309+fadkeabhi@users.noreply.github.com>
2023-03-12 09:08:27 +0530

    Create 12.cpp

---
+++ 2023/MAR/12.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    vector<int> findMaxRow(vector<vector<int>> mat, int N) 
+    {
+        vector<int> ans = {0,0};
+        int j;
+        for(int i=0;i<N;i++)
+        {
+            j = N-1-ans[1];
+            while(mat[i][j] == 1 and j>=0)
+            {
+                ans[0] = i;
+                j--;
+            }
+            ans[1] = N-1-j;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<vector<int>> arr(n, vector<int> (n));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cin >> arr[i][j];
+        Solution obj;
+        vector<int> ans = obj.findMaxRow(arr, n);
+        for(int val : ans) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
