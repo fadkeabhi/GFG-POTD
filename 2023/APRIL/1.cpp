338f01322ead8407daa298cb6d4287234ced6114
ABHISHEK FADAKE <31249309+fadkeabhi@users.noreply.github.com>
2023-04-01 11:40:06 +0530

    Create 1.cpp

---
+++ 2023/APRIL/1.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minOperations(int N) {
+        return (long)N*(long)N/4;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minOperations(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
